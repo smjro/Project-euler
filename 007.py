35e3f729d63ede415366c20c11e11a838298f367
smjro <smjro@example.com>
2019-04-07 12:04:30 +0900

    Add file

---
+++ 007.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,
+# we can see that the 6th prime is 13.
+# What is the 10001st prime number?
+# ---------------------------------------------------------
+# Answer: 104743
+
+import math
+
+
+def get_sieve_of_eratosthenes(n):
+    if not isinstance(n, int):
+        raise TypeError('n is int type.')
+    if n < 2:
+        raise ValueError('n is more than 2')
+    prime = []
+    limit = math.sqrt(n)
+    data = [i + 1 for i in range(1, n)]
+    while True:
+        p = data[0]
+        if limit <= p:
+            return prime + data
+        prime.append(p)
+        data = [e for e in data if e % p != 0]
+
+
+if __name__ == '__main__':
+    data = get_sieve_of_eratosthenes(1000000)
+    print("　".join(map(str, data)))
+    print(data[10000])
